2f3b005ccfc01d767ad7b9810f504defe8190fe2
Jianing Yang <jianingy.yang@gmail.com>
2013-04-10 17:10:30 +0800

    rename

--- source/scripts/runcamp.sh
+++ source/scripts/runcamp.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 PYTHON="$(which 2>/dev/null /home/tops/bin/python /usr/local/bin/python2 /usr/bin/python2 /static-python)"
-exec $PYTHON $(dirname $0)/camp.py $@
+exec $PYTHON $(dirname $0)/campcraft.py $@
